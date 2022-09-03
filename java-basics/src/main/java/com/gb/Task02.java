d6e067cffb111cdb6840106073a1ac64eac1974e
Eugene Menski <eugene.menski@gmail.com>
2022-09-03 19:28:16 +0300

    Find prefix that exists in every string

---
+++ java-basics/src/main/java/com/gb/Task02.java
@@ -0,0 +1,35 @@
+package main.java.com.gb;
+
+// Напишите функцию, для поиска наиболее длинного общего префикса, среди массива строк.
+// Если общего префикса нет, то возвращать пустую строку.
+// Пример ["aabb", "aabbb", "aaabb"] - ответ "aa"
+
+public class Task02 {
+    public static void main(String[] args) {
+        
+        String[] strings = new String[] {"aabb", "aabbb", "aaabb"};
+        
+        StringBuilder prefix = new StringBuilder();
+        int min_length = strings[0].length();
+        boolean check = true;
+
+        for (int i = 0; i < strings.length; i++){
+            if (strings[i].length() < min_length) {
+                min_length = strings[i].length();
+            }
+        }
+
+        for (int i = 0; i < min_length; i++) {
+            for (int j = 0; j < strings.length; j++) {
+                if (strings[0].charAt(i) != strings[j].charAt(i)) {
+                    check = false;
+                }
+            }
+            if (check == true) {
+                prefix.append(strings[0].charAt(i));
+            }
+        }
+
+        System.out.println("Общий префикс: " + prefix);
+    }
+}
