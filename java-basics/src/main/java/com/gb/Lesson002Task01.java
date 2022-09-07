eb32ab79ec904cabc6d2ed75dbd94367c5a47f82
Eugene Menski <eugene.menski@gmail.com>
2022-09-07 21:15:20 +0300

    Show list of folder's file extensions

--- java-basics/src/main/java/com/gb/Lesson002Task01.java
+++ java-basics/src/main/java/com/gb/Lesson002Task01.java
@@ -6,6 +6,30 @@
 
 package main.java.com.gb;
 
+import java.io.File;
+
 public class Lesson002Task01 {
-    
+    public static void main(String[] args) {
+        
+        File directory = new File("/Users/eugenemenski/Desktop/Unsorted/");
+
+        File[] files = directory.listFiles();
+
+        String message = "Расширение файла: ";
+        if (files != null && files.length > 0) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    System.out.println(message + "");
+                }
+                else {
+                    String extension = "";
+                    int i = file.getName().lastIndexOf('.');
+                    if (i >= 0) { // >= 0 чтобы отобразить, например, DS_Store
+                        extension = file.getName().substring(i+1);
+                    }
+                    System.out.println(message + extension);
+                }
+            }
+        }
+    }
 }
