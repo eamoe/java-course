a3e909951efae8f36d372f0c22a0524344b33ec0
Eugene Menski <eugene.menski@gmail.com>
2022-09-07 23:56:07 +0300

    Create sql-query

--- java-basics/src/main/java/com/gb/Lesson002Task02.java
+++ java-basics/src/main/java/com/gb/Lesson002Task02.java
@@ -5,8 +5,59 @@
 Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
 Ответ: "select * from students where name = 'Ivanov' and country = 'Russia' and city = 'Moscow'" для приведенного примера */
 
-package main.java;
+package main.java.com.gb;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
 public class Lesson002Task02 {
-    
+    public static void main(String[] args) {
+
+        String query = "SELECT * FROM students WHERE ";
+        String queryClause = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
+
+        Object jsonStr = JSONValue.parse(queryClause);
+
+        JSONObject obj = (JSONObject) jsonStr;
+        String name = (String) obj.get("name");
+        String country = (String) obj.get("country");
+        String city = (String) obj.get("city");
+        String age = (String) obj.get("age");
+
+        StringBuilder sqlQuery = new StringBuilder();
+        sqlQuery.append(query);
+
+        boolean after = false;
+
+        if (!name.equals("null")) {
+            sqlQuery.append("name = '" + name + "'");
+            after = true;
+        }
+
+        if(!country.equals("null")) {
+            if (after == true) {
+                sqlQuery.append(" AND ");
+            }
+            sqlQuery.append("country = '" + country + "'");
+            after = true;
+        }
+
+        if(!city.equals("null")) {
+            if (after == true) {
+                sqlQuery.append(" AND ");
+            }
+            sqlQuery.append("city = '" + city + "'");
+            after = true;
+        }
+
+        if(!age.equals("null")) {
+            if (after == true) {
+                sqlQuery.append(" AND ");
+            }
+            sqlQuery.append("age = '" + age + "'");
+        }
+
+        System.out.print(sqlQuery);
+
+    }
 }
