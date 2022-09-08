941d550ab7bfb83d98ccff3f06f33c8342678d9f
Eugene Menski <eugene.menski@gmail.com>
2022-09-08 10:22:09 +0300

    Parsed json and formed the response

--- java-basics/src/main/java/com/gb/Lesson002Task03.java
+++ java-basics/src/main/java/com/gb/Lesson002Task03.java
@@ -13,6 +13,43 @@
 
 package main.java.com.gb;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import java.util.Iterator;
+
 public class Lesson002Task03 {
-    
+    public static void main(String[] args) {
+
+        String jsonFile =   "[{\"фамилия\":\"Иванов\",\"оценка\":\"5\",\"предмет\":\"Математика\"},\n" +
+                            "{\"фамилия\":\"Петрова\",\"оценка\":\"4\",\"предмет\":\"Информатика\"},\n" +
+                            "{\"фамилия\":\"Краснов\",\"оценка\":\"5\",\"предмет\":\"Физика\"}]";
+
+        Object json = JSONValue.parse(jsonFile);
+        JSONArray array = (JSONArray) json;
+
+        Iterator it = array.iterator();
+        while(it.hasNext()){
+
+            JSONObject student = (JSONObject) it.next();
+            String lastName = (String) student.get("фамилия");
+            String score = (String) student.get("оценка");
+            String subject = (String) student.get("предмет");
+
+            StringBuilder response = new StringBuilder();
+            response.append("Студент ").append(lastName);
+            response.append(" получил ").append(score);
+            response.append(" по предмету ").append(subject);
+            response.append(".");
+
+            System.out.println(response);
+        }
+
+
+
+
+
+
+    }
 }
