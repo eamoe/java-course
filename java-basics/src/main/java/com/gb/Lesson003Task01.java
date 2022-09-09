dfbbe0eec8922d54334a9f2eb18f49fdbd0beb8b
Eugene Menski <eugene.menski@gmail.com>
2022-09-10 00:17:12 +0300

    Create list of integers and remove even numbers

--- java-basics/src/main/java/com/gb/Lesson003Task01.java
+++ java-basics/src/main/java/com/gb/Lesson003Task01.java
@@ -2,8 +2,33 @@
 
 package com.gb;
 
+import java.util.*;
+
 public class Lesson003Task01 {
     public static void main(String[] args) {
 
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество элементов: ");
+        int size = scanner.nextInt();
+
+        Random random = new Random();
+        for (int i = 0; i < size; i++){
+            numbers.add(random.nextInt(100));
+        }
+
+        System.out.println("Исходный список: " + numbers.toString());
+
+        int i = 0;
+        while (i < numbers.size()) {
+            if (numbers.get(i) % 2 == 0) {
+                numbers.remove(i);
+                i--;
+            }
+            i++;
+        }
+
+        System.out.println("Список после удаления четных чисел: " + numbers.toString());
     }
 }
