4d7355375835fbd07a984d4246de0b7ecb4d57ed
Eugene Menski <eugene.menski@gmail.com>
2022-09-11 19:34:07 +0300

    Added experiments

---
+++ java-basics/src/main/java/com/gb/Test.java
@@ -0,0 +1,20 @@
+package com.gb;
+
+public class Test {
+
+    enum Size {SMALL, MEDIUM, LARGE, EXTRA_LARGE}
+
+    // A constant that is available by any method of any class
+    // External availability through 'public' keyword
+    public static final double PI = 3.14;
+    public static void main(String[] args) {
+        final double CM_PER_INCH = 2.54; // Introduce a constant with 'final' keyword
+
+        Size size = Size.MEDIUM;
+
+        double x = 9.997;
+        // Correct way to use Round while converting to 'int' because Round converts to 'long' by default
+        int nx = (int) Math.round(x);
+        System.out.println((byte) 300);
+    }
+}
