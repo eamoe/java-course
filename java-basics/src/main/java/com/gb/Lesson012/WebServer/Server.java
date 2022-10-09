0f5bdacfac56efbacbad966612e27e6979915d22
Eugene Menski <eugene.menski@gmail.com>
2022-10-10 00:03:50 +0300

    web server

--- java-basics/src/main/java/com/gb/Lesson012/WebServer/Server.java
+++ java-basics/src/main/java/com/gb/Lesson012/WebServer/Server.java
@@ -1,26 +1,43 @@
-/* 3. Дописать простейший веб сервер, который начали писать на вебинаре,
+/* 3. Дописать простейший веб-сервер, который начали писать на вебинаре,
 чтобы он отправлял в качестве ответа на запрос содержимлое файла из HTTP-запроса.
 Подробная инструкция по этому заданию была в конце занятия */
 
 package com.gb.Lesson012.WebServer;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class Server {
 
+    public static String getResponseContent(File file) throws FileNotFoundException {
+        String responseContent;
+        if (!file.isFile()) {
+            responseContent = "Unfortunately, this page was not found!";
+        }
+        else {
+            StringBuilder builder = new StringBuilder();
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                builder.append(scanner.nextLine());
+            }
+            responseContent = builder.toString();
+        }
+        return responseContent;
+    }
+
     public static void main(String[] args) {
+
         try {
             ServerSocket serverSocket = new ServerSocket(8080);
+            System.out.println("Server has been started...");
 
-            for(;;) {
+            while (true) {
+                System.out.println("Waiting for a client...");
                 Socket socket = serverSocket.accept();
-                System.out.println("Client connected");
+                System.out.println("Client has been connected...");
                 BufferedReader reader = new BufferedReader(
                     new InputStreamReader(
                         socket.getInputStream(), StandardCharsets.UTF_8));
@@ -30,17 +47,41 @@ public class Server {
 
                 while (!reader.ready()) ;
 
+                System.out.println("Server has started reading...");
+                String requestedPage = "";
                 while (reader.ready()) {
-                    System.out.println(reader.readLine());
+                    String header = reader.readLine();
+                    if (header.split(" ")[0].equals("GET")) {
+                        requestedPage = header.split(" ")[1];
+                    }
+                    System.out.println(header);
+                }
+                System.out.println("Server has finished reading...");
+
+                File file = new File(new File(".") + requestedPage);
+                String content = getResponseContent(file);
+
+                int responseCode;
+                String response;
+                if (!file.exists()) {
+                    responseCode = 404;
+                    response = "Not Found";
+                }
+                else {
+                    responseCode = 200;
+                    response = "OK";
                 }
 
-                writer.println("HTTP/1.1 200 OK");
+                System.out.println("Server is responding...");
+                writer.println("HTTP/1.1" + " " + responseCode + " " + response);
                 writer.println("Content-Type: text/html; charset=utf-8");
                 writer.println();
-                writer.println("<h1>Hello world!!!</h1>");
+                writer.println(content);
                 writer.flush();
+                System.out.println("Server has responded...");
 
                 socket.close();
+                System.out.println("Socket has been closed...");
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
