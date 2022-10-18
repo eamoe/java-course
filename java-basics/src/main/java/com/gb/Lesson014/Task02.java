70393d126c2ea32b7bafc2149f77898d1a421e4a
Eugene Menski <eugene.menski@gmail.com>
2022-10-18 17:14:51 +0300

    Division

--- java-basics/src/main/java/com/gb/Lesson014/Task02.java
+++ java-basics/src/main/java/com/gb/Lesson014/Task02.java
@@ -7,5 +7,35 @@
 
 package com.gb.Lesson014;
 
+import java.util.Arrays;
+
 public class Task02 {
+
+    public static void main(String[] args) {
+
+        int[] array1 = {2, 3, 4, 5, 6, 7, 8};
+        int[] array2 = {1, 2, 3, 4, 5, 6, 7};
+
+        System.out.println(Arrays.toString(divide(array1, array2)));
+    }
+
+    public static float[] divide(int[] array1, int[] array2) {
+
+        if (array1 == null || array2 == null) {
+            throw new RuntimeException("Array is null");
+        }
+        else if (array1.length != array2.length) {
+            throw new RuntimeException("Arrays are of different size");
+        }
+
+        float[] result = new float[array1.length];
+        for (int i = 0; i < array1.length; i++) {
+            if (array2[i] == 0) {
+                throw new RuntimeException("Division by zero");
+            }
+            result[i] = (float) array1[i] / array2[i];
+        }
+        return result;
+    }
+
 }
