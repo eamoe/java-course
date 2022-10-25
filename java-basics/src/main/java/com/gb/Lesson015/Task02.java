5971856b16aa09a8d2326e4bb642555e6f0b8213
Eugene Menski <eugene.menski@gmail.com>
2022-10-25 14:40:28 +0300

    Completed task02

--- java-basics/src/main/java/com/gb/Lesson015/Task02.java
+++ java-basics/src/main/java/com/gb/Lesson015/Task02.java
@@ -4,4 +4,25 @@
 package com.gb.Lesson015;
 
 public class Task02 {
+
+    public static void catchException(int[] intArray) {
+        if (intArray == null) {
+            throw new RuntimeException("Array is null");
+        }
+        try {
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new RuntimeException("Index is out of bounds!");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+        catchException(array);
+    }
+
 }
