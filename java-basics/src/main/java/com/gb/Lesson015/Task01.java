ac7b9b92e976e3a55e22ae282d18e1c526ff7c13
Eugene Menski <eugene.menski@gmail.com>
2022-10-25 17:17:25 +0300

    Added get user input

--- java-basics/src/main/java/com/gb/Lesson015/Task01.java
+++ java-basics/src/main/java/com/gb/Lesson015/Task01.java
@@ -1,8 +1,32 @@
 /* Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float),
-и возвращает введенное значение. Ввод текста вместо числа не должно приводить к падению приложения,
+и возвращает введенное значение. Ввод текста вместо числа не должен приводить к падению приложения,
 вместо этого, необходимо повторно запросить у пользователя ввод данных. */
 
 package com.gb.Lesson015;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Task01 {
+
+    public static void getUserInput() {
+        float number;
+        boolean isCorrect = false;
+        while (!isCorrect) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Введите дробное число: ");
+            try {
+                number = scanner.nextFloat();
+                System.out.println("Было введено число: " + number);
+                isCorrect = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Некорректный формат числа! Попробуйте снова!");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        getUserInput();
+    }
+
 }
